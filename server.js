53da6eae2860ed794e55f5b4d5c5871bdaf950ff
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-11-22 23:05:58 +0700

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/books"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
